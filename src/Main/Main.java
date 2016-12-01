6ce23c670e701b4bd60f3fdefe93df8a12437f91
shiroiyuki <jack85055@gmail.com>
2016-12-01 15:51:01 +0800

    ButtonAction add exit(close jframe)

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,4 +1,5 @@
 package Main;
+
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
@@ -20,25 +21,32 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+
 public class Main extends JFrame implements ActionListener {
 	Toolkit tk = Toolkit.getDefaultToolkit();
 	int screenSizeX = (int) tk.getScreenSize().getWidth();
 	int screenSizeY = (int) tk.getScreenSize().getHeight();
 	Clip play;
-	JButton Exit = new JButton(new ImageIcon(""));
-	JButton Multi = new JButton(new ImageIcon(""));
-	JButton Room = new JButton(new ImageIcon(""));
+	//JButton Exit = new JButton(new ImageIcon(""));
+	JButton Exit = new JButton("Exit");
+	//JButton Multi = new JButton(new ImageIcon(""));
+	JButton Multi = new JButton("MultiPlay");
+	//JButton Room = new JButton(new ImageIcon(""));
+	JButton Room = new JButton("CreateRoom");
 	Menu menu = new Menu();
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new Main();
 	}
+
 	public Main() {
 
-		init(); //initialize
-		//start();
-		//multiplay();
+		init(); // initialize
+		// start();
+		// multiplay();
 	}
+
 	public void Gamesound() throws Exception {
 		File file = new File("");
 		//
@@ -51,6 +59,7 @@ public class Main extends JFrame implements ActionListener {
 		play.start();
 		play.loop(-1);
 	}
+
 	private void init() {
 		setTitle("Tower Denfense");
 		setResizable(false);
@@ -59,18 +68,22 @@ public class Main extends JFrame implements ActionListener {
 		setSize(screenSizeX, screenSizeY);
 		setJMenuBar(menu);
 		Room.setFont(new Font("SansSerif", Font.PLAIN, 18));
-		Room.setBounds(330, 250, 120, 50);
+		Room.setBounds(screenSizeX/2-125, 250, 250, 50);
+		Room.addActionListener(this);
 		Multi.setFont(new Font("SansSerif", Font.PLAIN, 18));
-		Multi.setBounds(330, 310, 120, 50);
+		Multi.setBounds(screenSizeX/2-125, 330, 250, 50);
+		Multi.addActionListener(this);
 		Exit.setFont(new Font("SansSerif", Font.PLAIN, 18));
-		Exit.setBounds(330, 370, 120, 50);
+		Exit.setBounds(screenSizeX/2-125, 410, 250, 50);
+		Exit.addActionListener(this);
 		add(Exit);
 		add(Multi);
 		add(Room);
 		setVisible(true);
 	}
-	private void start(){
-		//game panel
+
+	private void start() {
+		// game panel
 		tower tw = new tower();
 		tw.setHp(1000);
 		tw.setGold(100);
@@ -78,21 +91,21 @@ public class Main extends JFrame implements ActionListener {
 		JPanel pStart = new JPanel();
 		pStart.setSize(screenSizeX, screenSizeY);
 		add(pStart);
-		pStart.setLayout(null);//cancel all set
+		pStart.setLayout(null);// cancel all set
 
-		//my tower panel
+		// my tower panel
 		JPanel pMytower = new JPanel();
 		pMytower.setBounds(screenSizeX / 10, screenSizeY * 1 / 3, 200, 500);
 		pMytower.setBackground(Color.BLUE);
 		pStart.add(pMytower);
 
-		//emegy tower panel
+		// emegy tower panel
 		JPanel pEmeTower = new JPanel();
 		pEmeTower.setBounds(screenSizeX * 8 / 10, screenSizeY * 1 / 3, 200, 500);
 		pEmeTower.setBackground(Color.BLUE);
 		pStart.add(pEmeTower);
 
-		//my hp label
+		// my hp label
 		JLabel jMyHp = new JLabel(tw.getHp() + " / 1000");
 		jMyHp.setBounds(screenSizeX * 1 / 10, screenSizeY * 1 / 3 - 60, 200, 30);
 		jMyHp.setBackground(Color.red);
@@ -100,52 +113,54 @@ public class Main extends JFrame implements ActionListener {
 
 	}
 
-	public void multiplay(){
-		Server S = new Server();/*open server*/
-		Client C = new Client();/*client connect server*/
-		S.waitClient();/*server accept client connect*/
-		C.tran();/*client send HI! to server*/
+	public void multiplay() {
+		Server S = new Server();/* open server */
+		Client C = new Client();/* client connect server */
+		S.waitClient();/* server accept client connect */
+		C.tran();/* client send HI! to server */
 		try {
 			Thread.sleep(3000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}/*wait 3 second*/
-		S.get();/*server get packet and print*/
+		} /* wait 3 second */
+		S.get();/* server get packet and print */
 	}
 
-
-
-	class tower{
+	class tower {
 		private int hp;
-				int gold;
-				int armLavel;
-				int goldLavel;
+		int gold;
+		int armLavel;
+		int goldLavel;
 
-		public void setHp(int hp){
+		public void setHp(int hp) {
 			this.hp = hp;
 		}
+
 		public int getHp() {
 			return hp;
 		}
 
-		public void setGold(int gold){
+		public void setGold(int gold) {
 			this.gold = gold;
 		}
+
 		public int getGold() {
 			return gold;
 		}
 
-		public void armLavel(int armLavel){
+		public void armLavel(int armLavel) {
 			this.armLavel = armLavel;
 		}
+
 		public int getarmLavel() {
 			return armLavel;
 		}
 
-		public void goldLavel(int goldLavel){
+		public void goldLavel(int goldLavel) {
 			this.goldLavel = goldLavel;
 		}
+
 		public int getgoldLavel() {
 			return goldLavel;
 		}
@@ -155,9 +170,12 @@ public class Main extends JFrame implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-
+		if (e.getSource() == Exit) {
+			dispose();
+		}
 	}
 }
+
 class Menu extends JMenuBar {
 	JMenu system = new JMenu("System");
 	JMenuItem i1 = new JMenuItem("Save");
