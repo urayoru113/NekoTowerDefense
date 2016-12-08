72354b9252bd3fa0fab7420553df32809bf5d958
urayoru <hdhr1001@gmail.com>
2016-12-08 17:45:47 +0800

    replace

--- src/Main/Arms.java
+++ src/Main/Arms.java
@@ -1,20 +1,69 @@
 package Main;
 
 import java.util.ArrayList;
+
 /**
  * player can choose different kinds of arms
+ * 
  * @author jack
  *
- */
+ *******************************************/
 public class Arms {
-	ArrayList<Integer> aaa;
-	Arms()
-	{
-		
-	}
-	Arms(String race)
-	{
-		
+	private int hp;//Tower(player) hp
+	private int gold;
+	private int armLevel;//
+	private int goldLevel;//
+	private ArrayList<Neko> soldier;//add soldier(neko) into arrayList
+	//initialize
+	Arms() {
+		soldier = new ArrayList<>();
+		gold=100;
+		hp=1000;
+		goldLevel=1;
+		armLevel=1;
 	}
 
+	Arms(String race) {
+
+	}
+	//add soldier
+	public void addSoldier(int type) {
+		soldier.add(new Neko(type));
+	}
+
+	public void removeSoldier(int position) {
+
+	}
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+	public int getHp() {
+		return hp;
+	}
+
+	public void setGold(int gold) {
+		this.gold = gold;
+	}
+
+	public int getGold() {
+		return gold;
+	}
+
+	public void armLevel(int armLevel) {
+		this.armLevel = armLevel;
+	}
+
+	public int getarmLevel() {
+		return armLevel;
+	}
+
+	public void goldLevel(int goldLevel) {
+		this.goldLevel = goldLevel;
+	}
+
+	public int getgoldLevel() {
+		return goldLevel;
+	}
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,74 +1,144 @@
 package Main;
+
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.File;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-
-public class Main extends JFrame implements ActionListener {
-//aaaHI
+public class Main extends JFrame implements ActionListener,KeyListener,MouseListener {
 	Toolkit tk = Toolkit.getDefaultToolkit();
 	int screenSizeX = (int) tk.getScreenSize().getWidth();
 	int screenSizeY = (int) tk.getScreenSize().getHeight();
-
-	Server S;
-	Client C;
-
-	GuiSwing Gui = new GuiSwing();
+	// JButton Exit = new JButton(new ImageIcon(""));
+	JButton Exit = new JButton("Exit");
+	// JButton Multi = new JButton(new ImageIcon(""));
+	JButton Multi = new JButton("MultiPlay");
+	// JButton Room = new JButton(new ImageIcon(""));
+	JButton Room = new JButton("CreateRoom");
+	Menu menu = new Menu();
+	Clip play;
+	Image bufferImage;
+	Arms God=new Arms();
+	Arms enermy = new Arms();
+	Timer test;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new Main();
 	}
-	public Main() {
 
-		//init(); //initialize
-		//start();
-		multiplay();
+	public Main() {
+		init(); // initialize
+		start();
+		test = new Timer(1000,this);
+		//test.addActionListener(this);
+		test.start();
+		Graphics g = this.getGraphics();
+		while(true){
+			update(g);
+		}
+		// multiplay();
+	}
+	int i = 0;
+	public void paintFight(Graphics g) {
+		g.drawLine(0 , 120 , screenSizeX * 6 / 10, i++);
+		g.drawString("HI XD 你好 "+i,0 , 120);
+	}
+
+	public void update(Graphics g)// double buffer
+	{
+		bufferImage = createImage(screenSizeX / 10 * 6, screenSizeY*7/9);
+		Graphics gBuffer = bufferImage.getGraphics();
+		if (gBuffer != null)
+			paintFight(gBuffer);
+		else
+			paint(g);
+		gBuffer.dispose();
+	
+		g.drawImage(bufferImage, screenSizeX / 5, 80, null);
+	}
+
+	void Gamesound() throws Exception {
+		File file = new File("");
+		AudioInputStream sound = AudioSystem.getAudioInputStream(file);
+		DataLine.Info info = new DataLine.Info(Clip.class, sound.getFormat());
+		play = (Clip) AudioSystem.getLine(info);
+		play.open(sound);
+		play.start();
+		play.loop(-1);
 	}
 
 	private void init() {
 		setTitle("Tower Denfense");
 		setResizable(false);
+		setLayout(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(screenSizeX, screenSizeY);
-		setJMenuBar(Gui.menu);
+		setJMenuBar(menu);
+		Room.setFont(new Font("SansSerif", Font.PLAIN, 18));
+		Room.setBounds(screenSizeX / 2 - 125, 250, 250, 50);
+		Room.addActionListener(this);
+		Multi.setFont(new Font("SansSerif", Font.PLAIN, 18));
+		Multi.setBounds(screenSizeX / 2 - 125, 330, 250, 50);
+		Multi.addActionListener(this);
+		Exit.setFont(new Font("SansSerif", Font.PLAIN, 18));
+		Exit.setBounds(screenSizeX / 2 - 125, 410, 250, 50);
+		Exit.addActionListener(this);
+		//add(Exit);
+		// add(Multi);
+		// add(Room);
 		setVisible(true);
 	}
-	private void start(){
-		//game panel
-		tower tw = new tower();
-		tw.setHp(1000);
-		tw.setGold(100);
 
+	private void start() {
+		// game panel
 		JPanel pStart = new JPanel();
 		pStart.setSize(screenSizeX, screenSizeY);
 		add(pStart);
-		pStart.setLayout(null);//cancel all set
+		pStart.setLayout(null);// cancel all set
 
-		//my tower panel
+		// my tower panel
 		JPanel pMytower = new JPanel();
-		pMytower.setBounds(screenSizeX / 10, screenSizeY * 1 / 3, 200, 500);
+		pMytower.setBounds(screenSizeX / 10, screenSizeY * 1 / 3, screenSizeX /10, screenSizeY * 4 / 9);
 		pMytower.setBackground(Color.BLUE);
 		pStart.add(pMytower);
 
-		//emegy tower panel
+		// emegy tower panel
 		JPanel pEmeTower = new JPanel();
-		pEmeTower.setBounds(screenSizeX * 8 / 10, screenSizeY * 1 / 3, 200, 500);
+		pEmeTower.setBounds(screenSizeX * 8 / 10, screenSizeY * 1 / 3, screenSizeX / 10, screenSizeY * 4 / 9);
 		pEmeTower.setBackground(Color.BLUE);
 		pStart.add(pEmeTower);
 
-		//my hp label
-		JLabel jMyHp = new JLabel(tw.getHp() + " / 1000");
+		// my hp label
+		JLabel jMyHp = new JLabel(God.getHp() + " / 1000");
 		jMyHp.setBounds(screenSizeX * 1 / 10, screenSizeY * 1 / 3 - 60, 200, 30);
 		jMyHp.setBackground(Color.red);
 		pStart.add(jMyHp);
 
 	}
 
+<<<<<<< HEAD
 	public void multiplay(){
 
 
@@ -76,49 +146,129 @@ public class Main extends JFrame implements ActionListener {
 			S.start();
 
 			C = new Client();
+=======
+	public void multiplay() {
+		Server S = new Server();/* open server */
+		Client C = new Client();/* client connect server */
+		S.waitClient();/* server accept client connect */
+		C.tran();/* client send HI! to server */
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} /* wait 3 second */
+		S.get();/* server get packet and print */
+>>>>>>> ad874da0aabfb576f4dc69ed425180188405e61a
 	}
 
-	class tower{
-		private int hp;
-				int gold;
-				int armLavel;
-				int goldLavel;
+	/*
+	 * butoon action event and socket connect (non-Javadoc)
+	 * 
+	 * @see
+	 * java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 ********************************************************************************/
+	int cost = 0;
+	@Override
 
-		public void setHp(int hp){
-			this.hp = hp;
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource() == Exit) {
+			dispose();
 		}
-		public int getHp() {
-			return hp;
+	
+		if(e.getSource()==test){
+		
+			System.out.println(cost++);
 		}
-
-		public void setGold(int gold){
-			this.gold = gold;
-		}
-
-		public int getGold() {
-			return gold;
-		}
-
-		public void armLavel(int armLavel){
-			this.armLavel = armLavel;
-		}
-		public int getarmLavel() {
-			return armLavel;
-		}
-
-		public void goldLavel(int goldLavel){
-			this.goldLavel = goldLavel;
-		}
-		public int getgoldLavel() {
-			return goldLavel;
-		}
-
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) {
+	public void keyPressed(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}
+
+/*
+ * MenuBar-->
+ *********************************/
+class Menu extends JMenuBar {
+	JMenu system = new JMenu("System");
+	JMenuItem i1 = new JMenuItem("Save");
+	JMenuItem i2 = new JMenuItem("Load");
+	JMenuItem i3 = new JMenuItem("High Score");
+	JMenuItem i4 = new JMenuItem("Exit");
+
+	Menu() {
+		system.add(i1);
+		system.add(i2);
+		system.addSeparator();
+		system.add(i3);
+		system.add(i4);
+		add(system);
+		setOpaque(false);
+		setBackground(null);
+		setVisible(true);
+	}
+}
+
+/*
+ * BackGround setting game Frame
+ ********************/
+class BackgroundPanel extends JPanel {
+	Image im;
+
+	public BackgroundPanel(Image im) {
+		this.im = im;
+		this.setOpaque(true);
+	}
+
+	// Draw the back ground.
+	public void paintComponent(Graphics g) {
+		super.paintComponents(g);
+		g.drawImage(im, 0, 0, this.getWidth(), this.getHeight(), this);
+	}
 }
